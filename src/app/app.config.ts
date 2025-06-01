6dea27863ce7a3f204e003a0909a660dc227003b
Anthony Rainer <197171+pristinesource@users.noreply.github.com>
2025-05-31 22:49:04 -0500

    fixed for github pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ]
 };
